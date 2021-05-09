cacd2fb3abf701c32d83976a991c29aa7002afc9
talabax <fredya80@gmail.com>
2021-05-09 07:06:23 -0700

    added win/lose triggers

---
+++ Assets/scripts/LoseGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("You Lose");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/WinGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You win");
+            Destroy(other.gameObject);
+        }
+
+    }
+
+
+}
